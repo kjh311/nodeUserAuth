4f9a79112b2262e2f6cea28b6f79ad218458a44e
Kevin Huelsmann <kjh311@gmail.com>
2016-08-22 08:12:32 -0700

    user can now signup

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // will hold our database connection settings
 module.exports = {
 
-    // 'url' : mongodb://<kjh311>:<kevkevkev>@jello.modulusmongo.net:27017/pyq6Avud
+    'url' : 'mongodb://kjh311:kevkevkev@jello.modulusmongo.net:27017/pyq6Avud'
     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
