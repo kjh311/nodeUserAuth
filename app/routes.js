1c2c71ac1a4ab85c0cb87596057f3d7b6d17202b
Kevin Huelsmann <kjh311@gmail.com>
2016-08-23 09:44:56 -0700

    undo facebook auth

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,53 @@
 // all the routes for our application
 module.exports = function(app, passport) {
 
-    // route for home page
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
     app.get('/', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
 
-    // route for login form
-    // route for processing the login form
-    // route for signup form
-    // route for processing the signup form
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
 
-    // route for showing the profile page
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+     app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
@@ -19,24 +55,12 @@ module.exports = function(app, passport) {
     });
 
     // =====================================
-    // FACEBOOK ROUTES =====================
+    // LOGOUT ==============================
     // =====================================
-    // route for facebook authentication and login
-    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
-
-    // handle the callback after facebook has authenticated the user
-    app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
-        }));
-
-    // route for logging out
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
-
 };
 
 // route middleware to make sure a user is logged in
@@ -49,4 +73,3 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
-
