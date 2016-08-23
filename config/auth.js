e66c425418296b1aa2107b8273727920aa28912e
Kevin Huelsmann <kjh311@gmail.com>
2016-08-23 09:16:36 -0700

    adding facebook auth

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,23 @@
 // will hold all our client secret keys (facebook, twitter, google)
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1637776563179067', // your App ID
+        'clientSecret'  : 'c1cdc33f2f7ca92da048a54231d67830', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
